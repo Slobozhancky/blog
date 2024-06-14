99ac6df8ea788ea75f2f78d2d94c0eccbe97d757
Slobozhancky <il.slob93@gmail.com>
2024-06-14 12:58:05 +0300

    застосування патерна Singleton, для зєднання з базою

--- app/controllers/about.php
+++ app/controllers/about.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @var Database $db
+ */
+
 $title = "About";
 
 $content = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe nam iusto deleniti neque vero culpa dolore recusandae qui atque molestias exercitationem, repellendus cumque a aliquid cupiditate nemo, quos optio sed cum eveniet sint? Sint perferendis magni minus, sapiente provident deserunt?';
--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,7 +6,7 @@ $title = "Blog";
  * @var Database $db
  */
 
-$posts = $posts = $db->query("SELECT * FROM posts")->findAll();
+$posts = $db->query("SELECT * FROM posts")->findAll();
 $list_group = $db->query("SELECT * FROM `posts` ORDER BY `id` DESC LIMIT 3")->findAll();
 
 
